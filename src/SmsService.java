71158f4d698ac78c69f0cadb3b93b8118b880db5
aarora11 <arpit.arora89@live.com>
2017-07-14 23:29:06 +1000

    removing unused imports

--- src/SmsService.java
+++ src/SmsService.java
@@ -3,11 +3,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import com.fasterxml.jackson.core.type.TypeReference;
